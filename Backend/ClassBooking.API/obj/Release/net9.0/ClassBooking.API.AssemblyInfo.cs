90d5c83ef758f6ac71ee2941e47ecff985a6feb2
Vishnutharan <115442329+Vishnutharan@users.noreply.github.com>
2025-11-25 14:51:23 +0530

    AifixesOldVersion

--- Backend/ClassBooking.API/obj/Release/net9.0/ClassBooking.API.AssemblyInfo.cs
+++ Backend/ClassBooking.API/obj/Release/net9.0/ClassBooking.API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassBooking.API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e677e07dd3865a9e59694deb88cacfdbf9f5942f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassBooking.API")]
